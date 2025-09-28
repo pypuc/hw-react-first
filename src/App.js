68d37ef0f04e5b020bde401cb2f475329cbe64f3
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-09-28 16:56:23 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const myName = "Іван";
+  const favoriteSite = {
+    name: "Google",
+    url: "https://www.google.com/",
+  };
+  const num1 = 29;
+  const num2 = 50;
+  const sum = num1 + num2;
+   const colors = ["Чорний", "Фіолетовий", "Зелений"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>{myName}</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <img
+        src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQBBhx6-1bHI9nfO7iTlMkki_JffI3-Cl4kXs2Cetic9U54jwLGBD1QrrzNiLRBUAzFo1NPiGnMF3VVsr5SQeThDN-Ty6l6q-FMVbufOQ"
+        alt="дерево"
+      />
+       <p>
+        Улюблений сайт:{" "}
+        <a href={favoriteSite.url}>
+          {favoriteSite.name}
         </a>
-      </header>
+      </p>
+      <p>Сума чисел: {sum}</p>
+      <ul>
+        {colors.map((color, index) => (
+          <li key={index}>{color}</li>
+        ))}
+      </ul>
     </div>
   );
 }
